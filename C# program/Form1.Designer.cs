0268ffb8ae938b5f10a9b289f825a510103b72f3
諒弘 銭谷 <masahirozenitani@gmail.com>
2025-09-26 02:10:23 +0900

    C#

--- C# program/Form1.Designer.cs
+++ C# program/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -43,7 +44,10 @@
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
             this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -82,7 +86,7 @@
             this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.button1.Location = new System.Drawing.Point(440, 28);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 33);
+            this.button1.Size = new System.Drawing.Size(134, 33);
             this.button1.TabIndex = 3;
             this.button1.Text = "ファイル選択";
             this.button1.UseVisualStyleBackColor = false;
@@ -95,7 +99,7 @@
             this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.button2.Location = new System.Drawing.Point(440, 67);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(117, 34);
+            this.button2.Size = new System.Drawing.Size(134, 36);
             this.button2.TabIndex = 4;
             this.button2.Text = "表示";
             this.button2.UseVisualStyleBackColor = false;
@@ -107,7 +111,7 @@
             this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.button3.Location = new System.Drawing.Point(440, 140);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(117, 38);
+            this.button3.Size = new System.Drawing.Size(134, 38);
             this.button3.TabIndex = 5;
             this.button3.Text = "PUSH";
             this.button3.UseVisualStyleBackColor = false;
@@ -139,19 +143,25 @@
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(64, 431);
+            this.label3.AutoEllipsis = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(3, 24);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 18);
+            this.label3.Size = new System.Drawing.Size(230, 60);
             this.label3.TabIndex = 9;
-            this.label3.Text = "label3";
+            this.label3.Text = "00:00:00";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // button4
             // 
             this.button4.BackColor = System.Drawing.Color.Yellow;
-            this.button4.Location = new System.Drawing.Point(476, 254);
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button4.Location = new System.Drawing.Point(477, 254);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(117, 39);
+            this.button4.Size = new System.Drawing.Size(134, 39);
             this.button4.TabIndex = 10;
             this.button4.Text = "Yellow";
             this.button4.UseVisualStyleBackColor = false;
@@ -160,9 +170,9 @@
             // 
             this.button5.BackColor = System.Drawing.Color.Yellow;
             this.button5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.button5.Location = new System.Drawing.Point(476, 299);
+            this.button5.Location = new System.Drawing.Point(477, 299);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(117, 35);
+            this.button5.Size = new System.Drawing.Size(134, 35);
             this.button5.TabIndex = 11;
             this.button5.Text = "Green";
             this.button5.UseVisualStyleBackColor = false;
@@ -171,9 +181,9 @@
             // 
             this.button6.BackColor = System.Drawing.Color.Yellow;
             this.button6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.button6.Location = new System.Drawing.Point(476, 375);
+            this.button6.Location = new System.Drawing.Point(477, 375);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(117, 35);
+            this.button6.Size = new System.Drawing.Size(134, 35);
             this.button6.TabIndex = 12;
             this.button6.Text = "ファイル更新";
             this.button6.UseVisualStyleBackColor = false;
@@ -183,9 +193,9 @@
             this.button7.BackColor = System.Drawing.Color.Gray;
             this.button7.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
             this.button7.ForeColor = System.Drawing.Color.White;
-            this.button7.Location = new System.Drawing.Point(476, 442);
+            this.button7.Location = new System.Drawing.Point(477, 427);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(117, 33);
+            this.button7.Size = new System.Drawing.Size(127, 33);
             this.button7.TabIndex = 13;
             this.button7.Text = "PUSH";
             this.button7.UseVisualStyleBackColor = false;
@@ -194,33 +204,49 @@
             // 
             this.button8.BackColor = System.Drawing.Color.Yellow;
             this.button8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.button8.Location = new System.Drawing.Point(476, 340);
+            this.button8.Location = new System.Drawing.Point(477, 340);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(117, 33);
+            this.button8.Size = new System.Drawing.Size(133, 33);
             this.button8.TabIndex = 14;
             this.button8.Text = "Bule";
             this.button8.UseVisualStyleBackColor = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.label3_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(55, 375);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(249, 102);
+            this.panel1.TabIndex = 15;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(830, 500);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button8);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form①";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -240,6 +266,8 @@
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- C# program/Form1.cs
+++ C# program/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace C__program
 {
     public partial class Form1 : Form
     {
+
+       
         public Form1()
         {
             InitializeComponent();
@@ -26,5 +29,44 @@ namespace C__program
         {
 
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+            using (var brush = new System.Drawing.Drawing2D.LinearGradientBrush(
+                panel1.ClientRectangle,
+                Color.White,    // 上の色
+                Color.Blue,   // 下の色
+                System.Drawing.Drawing2D.LinearGradientMode.Vertical)) // 縦方向
+            {
+                e.Graphics.FillRectangle(brush, panel1.ClientRectangle);
+            }
+        }
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
